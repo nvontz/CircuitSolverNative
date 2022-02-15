6faeda517a68cd42467f144cedfd8d485471786f
Nathan Vontz <nvontz@ksu.edu>
2022-02-15 17:44:22 -0600

    added button to test but needs functionality

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Circuit Solver</Text>
+      <Button title='Test Server'/>
       <StatusBar style="auto" />
     </View>
   );
