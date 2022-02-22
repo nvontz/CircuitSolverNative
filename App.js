4fa0fcac0008425d187f67f8e8fc1469250b603c
Nathan Vontz <nvontz@ksu.edu>
2022-02-21 18:58:54 -0600

    added ability to take pictures and display them

--- App.js
+++ App.js
@@ -1,25 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { SafeAreaView } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
-  
-  console.log("App Executed");
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Circuit Solver</Text>
-      <Button onPress={() => alert("Hello Dr. Gruenbacher!")} 
-      title='Test Server'/>
-      <StatusBar style="auto" />
-    </View>
+      <Button
+        onPress={() => alert("Hello Dr. Gruenbacher!")}
+        title="Test Server"
+      />
+      <View style={{ flex: 1 }}>
+        <View style={styles.cameraContainer}>
+          <Camera
+            ref={(ref) => setCamera(ref)}
+            style={styles.fixedRatio}
+            type={type}
+            ratio={"4:3"}
+          />
+        </View>
+        <Button
+          title="Flip Image"
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        ></Button>
+        <Button title="Take Picture" onPress={() => takePicture()} />
+        {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
   },
 });
